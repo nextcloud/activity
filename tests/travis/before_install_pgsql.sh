0cb3ac6103cd186bd1ba2b79389f97915db8d988
Joas Schilling <nickvergessen@gmx.de>
2014-05-21 14:59:05 +0200

    Add bootstrap for tests

---
+++ tests/travis/before_install_pgsql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# ownCloud
+#
+# @author Thomas Müller
+# @copyright 2014 Thomas Müller thomas.mueller@tmit.eu
+#
+createuser -U travis -s oc_autotest
+
---
+++ tests/travis/before_install_sqlite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# ownCloud
+#
+# @author Thomas Müller
+# @copyright 2014 Thomas Müller thomas.mueller@tmit.eu
+#
+
