6486aee052462434de58cb58015cd65700162378
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-26 02:25:39 -0400

    [tx-robot] updated from transifex

--- l10n/ml_IN.php
+++ l10n/ml_IN.php
@@ -1,6 +1,23 @@
 <?php
 $TRANSLATIONS = array(
+"Your settings have been updated." => "നിങ്ങളുടെ താത്പര്യങ്ങൾ പരിഷ്കരിച്ചു",
+"Activity" => "പ്രവർത്തി",
+"You created %1\$s" => "നിങ്ങൾ %1\$s സൃഷ്ടിച്ചു",
+"%2\$s created %1\$s" => "%2\$s %1\$s സൃഷ്ടിച്ചു",
+"You changed %1\$s" => "നിങ്ങൾ %1\$s പരിഷ്കരിച്ചു",
+"%2\$s changed %1\$s" => "%2\$s %1\$s പരിഷ്കരിച്ചു",
+"You deleted %1\$s" => "നിങ്ങൾ %1\$s മായ്ച്ചു",
+"%2\$s deleted %1\$s" => "%2\$s %1\$s മായ്ച്ചു",
+"You shared %1\$s with %2\$s" => "നിങ്ങൾ %1\$s %2\$sനു പങ്കുവെച്ചു",
+"You shared %1\$s with group %2\$s" => "നിങ്ങൾ %1\$s %2\$s കൂട്ടത്തിന് പങ്കുവെച്ചു",
+"%2\$s shared %1\$s with you" => "%2\$s %1\$s നിങ്ങൾക്ക് പങ്കുവെച്ചു",
+"You shared %1\$s" => "നിങ്ങൾ %1\$s പങ്കുവെച്ചു",
+"Activity notification" => "പ്രവർത്തന അറിയിപ്പ്",
 "_%s and <strong class=\_tooltip\_ title=\_%s\_>%n more</strong>_::_%s and <strong class=\_tooltip\_ title=\_%s\_>%n more</strong>_" => array("",""),
-"_%s and %n more_::_%s and %n more_" => array("","")
+"_%s and %n more_::_%s and %n more_" => array("",""),
+"You will see a list of events here when you start to use your %s." => "നിങ്ങൾ നിങ്ങളുടെ %s ഉപയോഗിക്കാൻ  തുടങ്ങി കഴിയുമ്പോൾ കാര്യങ്ങളുടെ ഒരു പട്ടിക കാണാം",
+"Notifications" => "അറിയിപ്പ്",
+"Mail" => "കത്ത്",
+"Stream" => "പ്രവാഹം"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
