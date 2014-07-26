ba9e729edbe1ef5061fcec630d75974bd2a2805a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-26 02:25:34 -0400

    [tx-robot] updated from transifex

--- l10n/es_AR.php
+++ l10n/es_AR.php
@@ -1,7 +1,14 @@
 <?php
 $TRANSLATIONS = array(
+"Your settings have been updated." => "Tu configuración ha sido actualizada",
 "Activity" => "Actividad",
+"A file or folder has been <strong>shared</strong>" => "Un archivo o carpeta ha sido <strong>compartido</strong>",
+"A new file or folder has been <strong>created</strong>" => "Un archivo o carpeta ha sido <strong>creado</strong>",
+"A file or folder has been <strong>changed</strong>" => "Un archivo o carpeta ha sido <strong>modificado</strong>",
+"A file or folder has been <strong>deleted</strong>" => "Un archivo o carpeta ha sido <strong>eliminado</strong>",
 "Files" => "Archivos",
+"%s and %s" => "%s y %s",
+", " => ", ",
 "_%s and <strong class=\_tooltip\_ title=\_%s\_>%n more</strong>_::_%s and <strong class=\_tooltip\_ title=\_%s\_>%n more</strong>_" => array("",""),
 "_%s and %n more_::_%s and %n more_" => array("",""),
 "You will see a list of events here when you start to use your %s." => "Vas a ver una lista de eventos cuando empieces a usar tu %s.",
--- l10n/ta_IN.php
+++ l10n/ta_IN.php
@@ -1,5 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Your settings have been updated." => "உங்கள் அமைப்புகள் புதுப்பிக்கப்பட்டன.",
+"Activity" => "நடவடிக்கை",
+"A file or folder has been <strong>shared</strong>" => "ஒரு கோப்புறை அல்லது ஆவணம் <strong> பகிர்வு செய்யப்பட்டுள்ளது.</strong>",
+"A new file or folder has been <strong>created</strong>" => "ஒரு புதிய கோப்புறை அல்லது ஆவணம் <strong> உருவாக்கப்பட்டுள்ளது.</strong>",
+"A file or folder has been <strong>changed</strong>" => "ஒரு கோப்புறை அல்லது ஆவணம் <strong>மாற்றம் செய்யப்பட்டுள்ளது.</strong>",
+"A file or folder has been <strong>deleted</strong>" => "ஒரு கோப்புறை அல்லது ஆவணம்  <strong> நீக்கப்பட்டுள்ளது. </strong>",
+"You created %1\$s" => "நீங்கள் %1\$s 'ஐ உருவாக்கி உள்ளீர்கள். ",
+"%2\$s created %1\$s" => "%2\$s , %1\$s 'ஐ உருவாகியது. ",
+"You changed %1\$s" => "நீங்கள் %1\$s 'ல் மாற்றம் செய்து உள்ளீர்கள். ",
+"%2\$s changed %1\$s" => "%2\$s  %1\$s 'ல் மாற்றம் செய்துள்ளது.",
+"You deleted %1\$s" => "நீங்கள் %1\$s 'ஐ நீக்கி உள்ளீர்கள்.",
 "_%s and <strong class=\_tooltip\_ title=\_%s\_>%n more</strong>_::_%s and <strong class=\_tooltip\_ title=\_%s\_>%n more</strong>_" => array("",""),
 "_%s and %n more_::_%s and %n more_" => array("","")
 );
