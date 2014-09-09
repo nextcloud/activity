b830c77d77010d1e2ddff88cb39f6a11c16050c1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-09 02:25:17 -0400

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -8,6 +8,7 @@ $TRANSLATIONS = array(
 "A file or folder has been <strong>deleted</strong>" => "Un fichier ou un répertoire à été <strong>supprimé</strong>",
 "You created %1\$s" => "Vous avez créé %1\$s",
 "%2\$s created %1\$s" => "%2\$s a créé %1\$s",
+"%1\$s was created in a public folder" => "%1\$s a été créé dans un dossier public",
 "You changed %1\$s" => "Vous avez changé %1\$s",
 "%2\$s changed %1\$s" => "%2\$s a changé %1\$s",
 "You deleted %1\$s" => "Vous avez supprimé %1\$s",
