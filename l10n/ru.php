e3b6eb60532a1271c3afba17897a723d7a92d0cb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-25 02:25:18 -0400

    [tx-robot] updated from transifex

--- l10n/ru.php
+++ l10n/ru.php
@@ -13,6 +13,7 @@ $TRANSLATIONS = array(
 "%2\$s changed %1\$s" => "%2\$s изменено %1\$s",
 "You deleted %1\$s" => "Вы удалили %1\$s",
 "%2\$s deleted %1\$s" => "%2\$s удалено %1\$s",
+"You restored %1\$s" => "Вы восстановили %1\$s",
 "You shared %1\$s with %2\$s" => "Вы открыли доступ к %1\$s для %2\$s",
 "You shared %1\$s with group %2\$s" => "Вы открыли доступ к %1\$s группе %2\$s",
 "%2\$s shared %1\$s with you" => "%2\$s открыл Вам доступ к %1\$s",
