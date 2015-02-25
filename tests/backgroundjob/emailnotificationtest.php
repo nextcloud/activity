e13acc6b76669261d792f98eeb74e2aeb53d56f8
Joas Schilling <nickvergessen@gmx.de>
2015-02-25 15:49:33 +0100

    Add test coverage

--- tests/backgroundjob/emailnotificationtest.php
+++ tests/backgroundjob/emailnotificationtest.php
@@ -17,13 +17,73 @@ use OCA\Activity\BackgroundJob\EmailNotification;
 use OCA\Activity\Tests\TestCase;
 
 class EmailNotificationTest extends TestCase {
-	public function testExecute() {
-		$backgroundJob = new EmailNotification();
+	public function constructAndRunData() {
+		return [
+			[true],
+			[false],
+			[null],
+		];
+	}
+
+	/**
+	 * @dataProvider constructAndRunData
+	 *
+	 * @param bool $isCLI
+	 */
+	public function testConstructAndRun($isCLI) {
+		$backgroundJob = new EmailNotification(
+			$this->getMockBuilder('OCA\Activity\MailQueueHandler')->disableOriginalConstructor()->getMock(),
+			$this->getMock('OCP\IConfig'),
+			$this->getMock('OCP\ILogger'),
+			$isCLI
+		);
 
 		$jobList = $this->getMock('\OCP\BackgroundJob\IJobList');
 
 		/** @var \OC\BackgroundJob\JobList $jobList */
 		$backgroundJob->execute($jobList);
 		$this->assertTrue(true);
+	}
+
+	public function testRunStep() {
+		$mailQueueHandler = $this->getMockBuilder('OCA\Activity\MailQueueHandler')
+			->disableOriginalConstructor()
+			->getMock();
+		$config = $this->getMockBuilder('OCP\IConfig')
+			->disableOriginalConstructor()
+			->getMock();
+		$backgroundJob = new EmailNotification(
+			$mailQueueHandler,
+			$config,
+			$this->getMock('OCP\ILogger'),
+			true
+		);
+
+		$mailQueueHandler->expects($this->any())
+			->method('getAffectedUsers')
+			->with(2, 200)
+			->willReturn([
+				'test1',
+				'test2',
+			]);
+		$mailQueueHandler->expects($this->any())
+			->method('getItemsForUsers')
+			->willReturn([
+				'test1' => [],
+				'test2' => [],
+			]);
+		$config->expects($this->any())
+			->method('getUserValueForUsers')
+			->willReturnMap([
+				['settings', 'email', [
+					'test1',
+					'test2',
+				], [
+					'test1' => 'test1@localhost',
+					'test2' => '',
+				]]
+			]);
+
+		$this->assertEquals(2, \Test_Helper::invokePrivate($backgroundJob, 'runStep', [2, 200]));
 	}
 }
