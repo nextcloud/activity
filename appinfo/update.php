1657ff02cd1d489d3bbc17760c84e992e988402d
Joas Schilling <nickvergessen@owncloud.com>
2015-03-13 12:27:09 +0100

    Delete entries which are not a serialize or json_encode due to the old column length

--- appinfo/update.php
+++ appinfo/update.php
@@ -1,3 +1,21 @@
 <?php
 
-$installedVersion = OCP\Config::getAppValue('activity', 'installed_version');
+$installedVersion = \OCP\Config::getAppValue('activity', 'installed_version');
+
+if (version_compare($installedVersion, '1.2.2', '<')) {
+	$mistakes = [
+		['*PREFIX*activity', 'subjectparams'],
+		['*PREFIX*activity', 'messageparams'],
+		['*PREFIX*activity_mq', 'amq_subjectparams'],
+	];
+
+	foreach ($mistakes as $entry) {
+		list($table, $column) = $entry;
+		$query = \OCP\DB::prepare(
+			'DELETE FROM `' . $table . '` WHERE `' . $column . "` NOT LIKE '%]' AND `" . $column . "` NOT LIKE '%}'"
+		);
+		$numEntries = $query->execute();
+
+		\OC::$server->getLogger()->debug('Deleting ' . $numEntries . ' activities with a broken ' . $column . ' value.', ['app' => 'acitivity']);
+	}
+}
