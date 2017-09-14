3ddda2f94d1f8e8ab5eb67ff92a613911721de7d
Joas Schilling <coding@schilljs.com>
2017-09-14 12:48:32 +0200

    Follow naming guidelines

--- lib/Migration/Version2006Date20170808154933.php
+++ lib/Migration/Version2006Date20170808154933.php
@@ -5,7 +5,7 @@ use Doctrine\DBAL\Schema\Schema;
 use OCP\Migration\SimpleMigrationStep;
 use OCP\Migration\IOutput;
 
-class Version2006000Date20170808154933 extends SimpleMigrationStep {
+class Version2006Date20170808154933 extends SimpleMigrationStep {
 
 	/**
 	 * @param IOutput $output
