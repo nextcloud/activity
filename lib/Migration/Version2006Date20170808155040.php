f98fc3e794aa4d5e48edd0ef833bc732c5ea1aa8
Joas Schilling <coding@schilljs.com>
2018-01-17 11:38:52 +0100

    Fix migration type hints
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Migration/Version2006Date20170808155040.php
+++ lib/Migration/Version2006Date20170808155040.php
@@ -23,7 +23,7 @@
 
 namespace OCA\Activity\Migration;
 
-use Doctrine\DBAL\Schema\Schema;
+use OCP\DB\ISchemaWrapper;
 use OCP\Migration\SimpleMigrationStep;
 use OCP\Migration\IOutput;
 
@@ -31,14 +31,14 @@ class Version2006Date20170808155040 extends SimpleMigrationStep {
 
 	/**
 	 * @param IOutput $output
-	 * @param \Closure $schemaClosure The `\Closure` returns a `Schema`
+	 * @param \Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array $options
-	 * @return null|Schema
+	 * @return null|ISchemaWrapper
 	 * @throws \Doctrine\DBAL\Schema\SchemaException
 	 * @since 13.0.0
 	 */
 	public function changeSchema(IOutput $output, \Closure $schemaClosure, array $options) {
-		/** @var Schema $schema */
+		/** @var ISchemaWrapper $schema */
 		/**
 		 * FIXME To prevent slowness on update we don't change the index.
 		 * FIXME Anyone complaining can manually update it.
