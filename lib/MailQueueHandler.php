e681519da439c032e3389e84ea11e58bc6dce8c5
Joas Schilling <coding@schilljs.com>
2021-03-26 10:41:36 +0100

    Send the footer with the defined language
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/MailQueueHandler.php
+++ lib/MailQueueHandler.php
@@ -395,7 +395,7 @@ class MailQueueHandler {
 			$template->addBodyListItem($l->n('and %n more ', 'and %n more ', $skippedCount));
 		}
 
-		$template->addFooter();
+		$template->addFooter('', $lang);
 
 		$message = $this->mailer->createMessage();
 		$message->setTo([$email => $user->getDisplayName()]);
