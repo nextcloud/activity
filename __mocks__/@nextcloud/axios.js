54774a775f8d75ab35e31bcbeccae8449605c5be
Louis Chemineau <louis@chmn.me>
2023-12-18 12:22:09 +0100

    Fix HTTP request mock
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- __mocks__/@nextcloud/axios.js
+++ __mocks__/@nextcloud/axios.js
@@ -2,9 +2,9 @@ const axios = jest.createMockFromModule('@nextcloud/axios')
 
 const wsData = require('./activity_ws.json')
 
-axios.get = function(url) {
+axios.get = function (url) {
 	return new Promise((resolve, reject) => {
-		if (url === 'http://localhostundefined/ocs/v2.php/apps/activity/api/v2/activity/filter') {
+		if (url === 'http://localhost/ocs/v2.php/apps/activity/api/v2/activity/filter') {
 			resolve({ data: wsData })
 		} else {
 			// eslint-disable-next-line no-console
