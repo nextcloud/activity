87d79f1c847ceb8e6247692c5c820a809025856d
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-02 13:19:58 +0200

    Fix(lint): remove empty line
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- lib/Migration/Version2010Date20190416112817.php
+++ lib/Migration/Version2010Date20190416112817.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
  * SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
- 
 namespace OCA\Activity\Migration;
 
 use Closure;
