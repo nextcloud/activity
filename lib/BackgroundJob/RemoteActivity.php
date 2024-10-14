0b13ba88d4c107be031c0ed7e1cc410d1f7664e4
Julius Knorr <jus@bitgrid.net>
2024-10-14 08:16:25 +0200

    fix: Properly call parent constructor for remote activity job
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- lib/BackgroundJob/RemoteActivity.php
+++ lib/BackgroundJob/RemoteActivity.php
@@ -8,6 +8,7 @@ namespace OCA\Activity\BackgroundJob;
 
 use GuzzleHttp\Exception\ClientException;
 use OCA\Activity\Extension\Files;
+use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\BackgroundJob\QueuedJob;
 use OCP\Federation\ICloudId;
 use OCP\Federation\ICloudIdManager;
@@ -20,7 +21,8 @@ class RemoteActivity extends QueuedJob {
 	/** @var ICloudIdManager */
 	protected $cloudIdManager;
 
-	public function __construct(IClientService $clientService, ICloudIdManager $cloudIdManager) {
+	public function __construct(ITimeFactory $timeFactory, IClientService $clientService, ICloudIdManager $cloudIdManager) {
+		parent::__construct($timeFactory);
 		$this->clientService = $clientService;
 		$this->cloudIdManager = $cloudIdManager;
 	}
