df53c1be9052349c5604b724537c221be1950450
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-02-02 03:20:12 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- js/activity-api.mjs
+++ js/activity-api.mjs
@@ -1,2 +1,2 @@
 /*! third party licenses: js/vendor.LICENSE.txt */
-import{r as t}from"./api.chunk.mjs";import"./logger.chunk.mjs";import"./_commonjsHelpers.chunk.mjs";t();
+import{r as t}from"./api-aKAapiz4.chunk.mjs";import"./logger-CO4sLW2C.chunk.mjs";import"./_commonjsHelpers-BdswosdL.chunk.mjs";t();
