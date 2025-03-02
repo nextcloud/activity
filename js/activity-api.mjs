958bd147faae73ed6dfdf58bef1ce70570527281
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-03-02 03:29:12 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- js/activity-api.mjs
+++ js/activity-api.mjs
@@ -1,1 +1,1 @@
-import{r as t}from"./api-Bbja46ws.chunk.mjs";import"./logger-DdfSSqJ9.chunk.mjs";import"./_commonjsHelpers-D9DevfhH.chunk.mjs";t();
+import{r as t}from"./api-CKODZZjL.chunk.mjs";import"./logger-ByCR-oAD.chunk.mjs";import"./_commonjsHelpers-D9DevfhH.chunk.mjs";t();
