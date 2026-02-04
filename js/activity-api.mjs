0952b0acdacdccc7a3d48cfab7c01b871597243f
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-02-04 21:24:30 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/activity-api.mjs
+++ js/activity-api.mjs
@@ -1,2 +1,2 @@
-import{r as t}from"./api-CnytGYOr.chunk.mjs";import"./logger-X0KjIiP2.chunk.mjs";import"./_commonjsHelpers-Cqwegr3R.chunk.mjs";t();
+import{r as t}from"./api-Di1aL-dn.chunk.mjs";import"./logger-X0KjIiP2.chunk.mjs";import"./_commonjsHelpers-Cqwegr3R.chunk.mjs";t();
 //# sourceMappingURL=activity-api.mjs.map
