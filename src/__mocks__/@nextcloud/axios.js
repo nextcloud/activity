ebb93b3f48171df88c1176a7c9ef84c46ac0b8fe
Anna Larch <anna@nextcloud.com>
2026-04-08 19:07:25 +0200

    feat: add download counter to activity sidebar
    Signed-off-by: Anna Larch <anna@nextcloud.com>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- src/__mocks__/@nextcloud/axios.js
+++ src/__mocks__/@nextcloud/axios.js
@@ -8,11 +8,14 @@ import wsData from './activity_ws.json' with { type: 'json' }
 const axios = {
 	/**
 	 * @param {string} url URL to get
+	 * @param {object} [config] Axios config
 	 */
-	get(url) {
+	get(url, config) {
 		return new Promise((resolve, reject) => {
 			if (url.endsWith('/ocs/v2.php/apps/activity/api/v2/activity/filter')) {
 				resolve({ data: wsData })
+			} else if (url.endsWith('/ocs/v2.php/apps/activity/api/v2/activity/downloads/count')) {
+				resolve({ data: { ocs: { meta: { status: 'ok', statuscode: 200 }, data: { total: 10, last30d: 3 } } } })
 			} else {
 				reject(new Error(`URL not defined ${url}`))
 			}
