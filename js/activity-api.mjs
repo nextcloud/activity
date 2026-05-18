0f6246d314cd406ac4ecd25f2f6e10c474e85560
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-05-18 11:31:56 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/activity-api.mjs
+++ js/activity-api.mjs
@@ -1,2 +1,2 @@
-import{r as t}from"./api-Ece4zLIo.chunk.mjs";import"./logger-BgVM16Kc.chunk.mjs";import"./index-DxSbrVwz.chunk.mjs";t();
+import{r as t}from"./api-Cy27h5PI.chunk.mjs";import"./logger-VyEVPn8K.chunk.mjs";import"./index-DxSbrVwz.chunk.mjs";t();
 //# sourceMappingURL=activity-api.mjs.map
---
+++ js/logger-VyEVPn8K.chunk.mjs
@@ -0,0 +1,2 @@
+import{c as t}from"./index-DxSbrVwz.chunk.mjs";const c=t().setApp("activity").detectUser().build();export{c as l};
+//# sourceMappingURL=logger-VyEVPn8K.chunk.mjs.map
